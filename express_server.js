b302924ba1c526100b05345d73a8b477faf0d920
Oluwatobii <bellotobi10@gmail.com>
2020-07-06 23:35:43 +0000

    Add a POST route to recieve the form submission

--- express_server.js
+++ express_server.js
@@ -4,10 +4,23 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  let result = Math.random().toString(36).substring(2, 8);
+  return result;
+}
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.post("/urls", (req, res) => {
+//   console.log(req.body); // Log the POST request body to the console
+//   res.send("Ok"); // Respond with 'Ok' (we will replace this)
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello!");
