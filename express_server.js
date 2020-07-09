510d13f0127d1b793008e76d6eb50ee94cb206dc
Oluwatobii <bellotobi10@gmail.com>
2020-07-09 00:02:04 +0000

    Registering new users

--- express_server.js
+++ express_server.js
@@ -4,8 +4,24 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -21,9 +37,40 @@ function generateRandomString() {
   return result;
 }
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+const addNewUser = (email, password) => {
+  // generate an id
+  const userId = generateRandomString();
+  // create a new user object
+  const newUser = {
+    id: userId,
+    email,
+    password,
+  };
+  users[userId] = newUser;
+
+  return userId;
+};
+
+const findTheUserByEmail = (email) => {
+  for (let userId in users) {
+    if (users[userId].email === email) {
+      return users[userId];
+    }
+  }
+  return false;
+};
+
+const authenticateUser = (email, password) => {
+  // Does the user with that email exist?
+  const user = findTheUserByEmail(email);
+
+  // check the email and passord match
+  if (user && user.password === password) {
+    return user.id;
+  } else {
+    return false;
+  }
+};
 
 app.post("/urls", (req, res) => {
   const newShortURL = generateRandomString();
@@ -47,15 +94,27 @@ app.post("/urls/:shortURL/update", (req, res) => {
 app.post("/login", (req, res) => {
   let credentials = req.body.username;
 
-  res.cookie("username", credentials);
-  console.log("You're here");
+  res.cookie("user_id", credentials);
+  //console.log("You're here");
 
   res.redirect(`/urls`);
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect(`/urls`);
+});
+
+app.get("/urls", (req, res) => {
+  if (!req.cookies.user_id) {
+    res.redirect("/register");
+  } else {
+    const templateVars = {
+      urls: urlDatabase,
+      email: users[req.cookies.user_id].email,
+    };
+    res.render("urls_index", templateVars);
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -70,21 +129,44 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
-  res.render("urls_index", templateVars);
-});
-
 app.get("/urls/new", (req, res) => {
-  let templateVars = {
-    username: req.cookies["username"],
+  const templateVars = {
+    email: users[req.cookies.user_id].email,
   };
   res.render("urls_new", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { email: "" };
+
+  res.render("urls_registration", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  //console.log("REQ.BODY===>", req.body);
+  const { email, password } = req.body;
+  if (email === "" || password === "") {
+    res.status(400).send("Email & Password needs to be filled out!");
+  }
+
+  //console.log("USERS===>", users);
+  const userExistInDb = findTheUserByEmail(email);
+
+  if (userExistInDb) {
+    res.status(400).send("Error: email already exists");
+  } else {
+    const userId = addNewUser(email, password);
+    //console.log("USERS===>", users);
+
+    res.cookie("user_id", userId);
+    res.redirect("/urls");
+  }
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = {
-    username: req.cookies["username"],
+  console.log(req.cookies);
+  const templateVars = {
+    email: users[req.cookies.user_id].email,
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
